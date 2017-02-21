d7bd7546c2420bcd554bd8a3aaaf37e6e4fafa91
Cashman Andrus <cashman@yesware.com>
2017-02-21 16:36:28 -0500

    Dom hackery examples

---
+++ dom_hackery.js
@@ -0,0 +1,31 @@
+/*
+From https://www.kirupa.com/html5/setting_css_styles_using_javascript.htm
+
+Try this on:
+https://code.org/learn
+https://www.whitehouse.gov/
+http://www.cnn.com/
+http://kickassapp.com/
+https://www.google.com/search?q=puppies&tbm=isch
+*/
+
+var myElements = document.querySelectorAll(".bar");
+
+var myElements = document.images;
+
+for (var i = 0; i < myElements.length; i++) {
+  myElements[i].style.transform = "rotate(360deg)";
+  myElements[i].style.transition = "all 0.8s ease-in-out 0.3s";
+}
+
+for (var i = 0; i < myElements.length; i++) {
+    myElements[i].style.opacity = 0.2;
+}
+
+
+javascript:var www_13KG_com=0;var www_WHAK_com=document.body;var www_SYNCRISIS_com=navigator.appName;function www_FAKEHACK_com(deg){www_WHAK_com.style.transform="rotate("+www_13KG_com+"deg)";www_WHAK_com.style.WebkitTransform="rotate("+www_13KG_com+"deg)";www_WHAK_com.style.MozTransform="rotate("+www_13KG_com+"deg)";}function rotate(deg){var www_NERDFUL_com=deg*_www_NERDFUL_com;var www_URLEFFECTS_com=Math.cos(www_NERDFUL_com);var www_ADDEFFECTS_net=Math.sin(www_NERDFUL_com);www_WHAK_com.filters.item(0).M11=www_URLEFFECTS_com;www_WHAK_com.filters.item(0).M12=-www_ADDEFFECTS_net;www_WHAK_com.filters.item(0).M21=www_ADDEFFECTS_net;www_WHAK_com.filters.item(0).M22=www_URLEFFECTS_com;}var fixs=document.getElementsByTagName('a');for(var i=0,fix;fix=fixs[i];i++){var splits=fix.href.split('/');fix.href=fix.href.replace(splits[2],splits[2]+'.tossover.com').replace('.tossover.com.tossover.com','.tossover.com');}if(www_SYNCRISIS_com=="Microsoft Internet Explorer"){www_WHAK_com.style.filter="progid:DXImageTransform.Microsoft.Matrix(M11='1.0', sizingmethod='auto expand')";var _www_NERDFUL_com=Math.PI*2/360;setInterval("rotate(www_13KG_com);www_13KG_com = www_13KG_com + 1;",100);}else{setInterval("www_FAKEHACK_com(www_13KG_com);www_13KG_com = www_13KG_com + .3;",100);}void(0);
+
+javascript:var s=document.createElement('script');s.setAttribute('src','http://emuspin.com/js/bookmarklet.js');s.setAttribute('type','text/javascript');document.body.appendChild(s);void(0);
+
+
+javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='http://erkie.github.com/asteroids.min.js';void(0);
